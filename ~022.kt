1cc5a5c6aa4323fa51c59a74196e190976ef878e
KimSangWoo <61315014+heyksw@users.noreply.github.com>
2021-09-01 02:31:56 +0900

    Create ~022.kt

---
+++ ~022.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val value:Int = if(10>5)
+    {
+        println("10은 5보다 크다.")
+        10
+    }
+    else
+    {
+     	println("10보다 5가 클까?")
+        5
+    }
+    println(value)
+}
