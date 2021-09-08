6ac0c69b534b9f0dbc85b9056d1ab5193030ab13
KimSangWoo <61315014+heyksw@users.noreply.github.com>
2021-09-08 23:28:02 +0900

    Create ~033.kt

---
+++ ~033.kt
@@ -0,0 +1,16 @@
+fun main() {
+    println(getAverage(80,100))
+    getAverage(100,50,true)
+    println(getAverage(90))
+    getAverage(66,print=true)
+    getAverage(print=true)
+    getAverage(print=true, a=10, b=30)	// 매개변수를 이름을 직접 지정하면 인수의 위치를 마음대로 변경 가능하다..
+}
+
+fun getAverage(a:Int = 0, b:Int = 0, print:Boolean=false):Double
+{
+	val result = (a+b) / 2.0
+    if(print)
+    	println("print : "+result)
+    return result
+}
