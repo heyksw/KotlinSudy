c88ea4f820d86831feff711263a7f78023c3763f
KimSangWoo <61315014+heyksw@users.noreply.github.com>
2021-10-01 22:20:31 +0900

    Create ~047.kt

---
+++ ~047.kt
@@ -0,0 +1,17 @@
+class Person
+{
+    var name:String = ""
+    var age:Int = 0
+}
+
+fun main(){
+
+    val person:Person = Person()
+    person.name = "김상우"
+    person.age = 26
+
+    val person2:Person = Person()
+    person.name = "배서진"
+    person.age = 26
+}
+
